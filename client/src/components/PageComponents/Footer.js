1b57344c732774baf9a5a1b83289d1e9726f8cfc
Jac White <jac.white99@gmail.com>
2020-03-15 00:28:47 +0000

    footer redesigned

--- client/src/components/PageComponents/Footer.js
+++ client/src/components/PageComponents/Footer.js
@@ -1,7 +1,42 @@
 import React from "react";
 
+//Social Media Icons
+import Facebook from "../../images/facebook-icon.png";
+
 const footer = () => {
-	return <div className="footer">Footer Design Here</div>;
+	return (
+		<div className="footer">
+			<p>
+				<img
+					src={Facebook}
+					alt="facebook-icon"
+					style={{ width: "1.5vw" }}
+				></img>
+				<a href="https://www.facebook.com/Foxdale-Guesthouse-and-Campsite-108245277376752/">
+					Facebook Page
+				</a>
+			</p>
+			<p className="footer__links">
+				<a className="footer__link" href="/">
+					Home
+				</a>
+				<a className="footer__link" href="/contact">
+					Contact
+				</a>
+				<a className="footer__link" href="/rooms">
+					Rooms
+				</a>
+				<a className="footer__link" href="/rooms/guesthouse">
+					Guesthouse
+				</a>
+				<a className="footer__link" href="/rooms/campsite">
+					Campsite
+				</a>
+			</p>
+			<p className="footer__border"></p>
+			<h1 className="footer__title">FOXDALE</h1>
+		</div>
+	);
 };
 
 export default footer;
